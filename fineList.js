7868ac7f9ac8c123d118a26bbc665d5d46cb0635
KateNa12 <60751131+KateNa12@users.noreply.github.com>
2024-02-05 11:50:28 +0200

    Create fineList.js

---
+++ fineList.js
@@ -0,0 +1,31 @@
+"use strict";
+window.fineList = {
+    searchFines : searchFines
+}
+
+//Ця зміна містить всі дані які в нас зберігаються у файлі data
+let DB = data.finesData;
+
+console.log(true+false);
+function searchFines(searchKey){
+    /*
+     Напишіть свій код тут!
+     Як ви бачите функція повертає статичні дані.
+     Замість масиву який прописаний хардкодом, вам необхідно реалізувати цю функцію
+     так, щоб вона повертала масив відповідно переданому значенню в функцію.
+     Саме значення - це "Пошук за номером" або "Пошук за типом штрафу"
+     Тип штрафу може бути тільки
+     - Перевищення швидкості
+     - Невірне паркування
+     - Їзда у не тверезому стані
+     */
+    
+    var num = parseInt(searchKey);
+    var result;
+    if (num == searchKey) {
+        result = DB.filter((fine) => parseInt(fine.номер) == num )
+    } else {
+        result = DB.filter((fine) => fine.тип == searchKey )
+    }
+    return result;
+}
